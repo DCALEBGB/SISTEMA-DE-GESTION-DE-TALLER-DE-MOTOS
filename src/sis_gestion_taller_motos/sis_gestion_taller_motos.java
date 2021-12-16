e6eaa5870ed1809153883ce205361a10d1e3e8c6
PC <PC@192.168.1.4>
2021-12-16 00:37:00 -0500

    CASI FINAL 4

---
+++ src/sis_gestion_taller_motos/sis_gestion_taller_motos.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sis_gestion_taller_motos;
+
+import Formulario.frm_Login;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class sis_gestion_taller_motos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frm_Login lg = new frm_Login();
+        lg.setVisible(true);
+    }
+    
+}
